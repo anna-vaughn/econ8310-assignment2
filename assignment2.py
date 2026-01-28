5389a096bd93f7b8438acc71e9003a615e71655d
anna-vaughn <60945664+anna-vaughn@users.noreply.github.com>
2026-01-27 18:42:13 -0600

    Submission attempt 6

--- assignment2.py
+++ assignment2.py
@@ -3,20 +3,13 @@ from sklearn.tree import DecisionTreeClassifier
 
 # Get data.
 data = pd.read_csv("https://github.com/dustywhite7/Econ8310/raw/master/AssignmentData/assignment3.csv")
-data['DateTime'] = pd.to_datetime(data['DateTime']) # Convert data type to datetime.
-data['hour'] = data['DateTime'].dt.hour # Get only hour.
-data['day'] = data['DateTime'].dt.day # Get day (date).
 
 # Get testing data.
 ## Meal is null, waiting to be filled in.
 predData = pd.read_csv("https://github.com/dustywhite7/Econ8310/raw/master/AssignmentData/assignment3test.csv")
-predData['DateTime'] = pd.to_datetime(predData['DateTime'])
-predData['hour'] = predData['DateTime'].dt.hour
-predData['month'] = predData['DateTime'].dt.month
-predData['day'] = predData['DateTime'].dt.day
 
 # Designate dependent (y) and independent (y) vars.
-x = data[['Total', 'Discounts', 'hour', 'month' 'day']]
+x = data.drop(['id', 'DateTime', 'meal'], axis=1) # Drop string, timestamp, and dependent var.
 y = data['meal']
 
 # Create XGBoost model.
@@ -30,5 +23,5 @@ model = XGBClassifier(
 modelFit = model.fit(x,y)
 
 # Test our model using the testing data
-predData = predData[['Total', 'Discounts', 'hour', 'month', 'day']]
+predData = predData.drop(['id', 'DateTime', 'meal'], axis=1)
 pred = modelFit.predict(predData)
